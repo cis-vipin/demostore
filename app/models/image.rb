dc3641bcaf5198d906ac555805827a8e7ef40705
arun <arun@warp9inc.com>
2013-12-20 18:14:45 +0530

    updat

---
+++ app/models/image.rb
@@ -0,0 +1,9 @@
+class Image < ActiveRecord::Base
+  # attr_accessible :title, :body
+
+  attr_accessible :image
+  belongs_to :imageable, polymorphic: true
+
+  mount_uploader :image , ImageUploader
+
+end
